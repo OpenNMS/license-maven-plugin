657b2c789ed84884a0e7e2842ce5d4e529e8433e
Benjamin Reed <ranger@opennms.org>
2023-03-29 15:45:42 -0400

    tabs to spaces

--- src/it/ISSUE-135/src/main/original/java/org/example/test/InvalidLicenseHeader.java
+++ src/it/ISSUE-135/src/main/original/java/org/example/test/InvalidLicenseHeader.java
@@ -2,8 +2,8 @@ package org.example.test;import java.io.File;
 
 
 public class InvalidLicenseHeader {
-	public static void main(String[] args) {
-		File file = new File(args[0]);
-		System.out.println(file.getAbsolutePath());
-	}
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        System.out.println(file.getAbsolutePath());
+    }
 }
